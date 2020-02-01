4dd050548a7fecbd7a50ef0862442cd41a042a85
Riley Denoyer <radbuglet@gmail.com>
2020-02-01 09:46:52 -0800

    Implement simple camera controller; establish base structure

---
+++ game/CameraController.cs
@@ -0,0 +1,18 @@
+using Godot;
+using Refactorio.helpers;
+
+namespace Refactorio.game
+{
+    public class CameraController : Camera
+    {
+        public override void _Process(float delta)
+        {
+            var direction = new Vector3();
+            if (Input.IsActionPressed(ActionNames.MoveCameraLeftAction)) direction.x += -1;
+            if (Input.IsActionPressed(ActionNames.MoveCameraRightAction)) direction.x += 1;
+            if (Input.IsActionPressed(ActionNames.MoveCameraUpAction)) direction.z += -1;
+            if (Input.IsActionPressed(ActionNames.MoveCameraDownAction)) direction.z += 1;
+            Translate(direction * delta * 20); 
+        }
+    }
+}
---
+++ helpers/ActionNames.cs
@@ -0,0 +1,10 @@
+namespace Refactorio.helpers
+{
+    public static class ActionNames
+    {
+        public const string MoveCameraLeftAction = "cam_left";
+        public const string MoveCameraRightAction = "cam_right";
+        public const string MoveCameraUpAction = "cam_up";
+        public const string MoveCameraDownAction = "cam_down";
+    }
+}
