a2cb7291a138d9f72a90112388e24d4b3bc5cb76
Reconcyl <>
2020-02-01 12:42:29 -0800

    add warningy runtime code

--- game/scripting/Runtime.cs
+++ game/scripting/Runtime.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Linq;
-using Godot;
 using Godot.Collections;
 using Refactorio.helpers;
 
@@ -7,21 +7,138 @@ namespace Refactorio.game.scripting
 {
     public class Runtime
     {
-        private interface Expression
+        public interface IExpression
         {
             int Evaluate(Runtime runtime);
         }
-        private interface IReferentialExpression : Expression
+        public interface IReferentialExpression : IExpression
         {
             void Assign(Runtime runtime, int value);
         }
-        private interface IInstruction
+
+        public class LiteralExpression : IExpression
+        {
+            public int value;
+
+            public int Evaluate(Runtime runtime)
+            {
+                return value;
+            }
+        }
+
+        public class IndexExpression : IReferentialExpression
+        {
+            public IExpression index;
+            public int Evaluate(Runtime runtime)
+            {
+                var idx = index.Evaluate(runtime);
+                return runtime.GetMemory(idx);
+            }
+
+            public void Assign(Runtime runtime, int value)
+            {
+                var idx = index.Evaluate(runtime);
+                runtime.SetMemory(idx, value);
+            }
+        }
+
+        public class VariableExpression : IReferentialExpression
+        {
+            public string name;
+            
+            public int Evaluate(Runtime runtime)
+            {
+                return runtime.GetVariable(name);
+            }
+
+            public void Assign(Runtime runtime, int value)
+            {
+                runtime.SetVariable(name, value);
+            }
+        }
+
+        public enum UnaryOperator
+        {
+            Abs,
+            Minus,
+            Not,
+        }
+
+        public class UnaryExpression : IExpression
+        {
+            public IExpression inner;
+            public UnaryOperator op;
+
+            public int Evaluate(Runtime runtime)
+            {
+                var a = inner.Evaluate(runtime);
+                switch (op)
+                {
+                    case UnaryOperator.Abs:
+                        return Math.Abs(a);
+                    case UnaryOperator.Minus:
+                        return -a;
+                    case UnaryOperator.Not:
+                        return a == 0 ? 1 : 0;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public enum BinaryOperator
+        {
+            Plus,
+            Minus,
+            Times,
+            Div,
+            Mod,
+            Eq,
+            And,
+            Or,
+        }
+
+        public class BinaryExpression : IExpression
+        {
+            public IExpression left;
+            public IExpression right;
+            public BinaryOperator op;
+
+            public int Evaluate(Runtime runtime)
+            {
+                var a = left.Evaluate(runtime);
+                var b = right.Evaluate(runtime);
+                switch (op)
+                {
+                    case BinaryOperator.Plus:
+                        return a + b;
+                    case BinaryOperator.Minus:
+                        return a - b;
+                    case BinaryOperator.Times:
+                        return a * b;
+                    case BinaryOperator.Div:
+                        return b == 0 ? 0 : a / b;
+                    case BinaryOperator.Mod:
+                        return b == 0 ? 0 : a % b;
+                    case BinaryOperator.Eq:
+                        return a == b ? 1 : 0;
+                    case BinaryOperator.And:
+                        return a != 0 && b != 0 ? 1 : 0;
+                    case BinaryOperator.Or:
+                        return a != 0 || b != 0 ? 1 : 0;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public interface IInstruction
         {
             void Execute(Runtime runtime);
         }
-        private class AssignmentInstruction : IInstruction
+        public class AssignmentInstruction : IInstruction
         {
-            private Expression source;
+            private IExpression source;
             private IReferentialExpression destination;
 
             public void Execute(Runtime runtime)
@@ -30,18 +147,18 @@ namespace Refactorio.game.scripting
             }
         }
 
-        private class EventInstruction : IInstruction
+        public class EventInstruction : IInstruction
         {
-            private string eventName;
+            public string eventName;
             public void Execute(Runtime runtime)
             {
-                
+                runtime.RunEvent(eventName);
             }
         }
-        private class Condition
+        public class Condition
         {
-            private bool checksZero;
-            private string variable;
+            public bool checksZero;
+            public string variable;
 
             public bool IsMet(Runtime runtime)
             {
@@ -55,10 +172,10 @@ namespace Refactorio.game.scripting
                 }
             }
         }
-        private class ConditionalInstruction
+        public class ConditionalInstruction
         {
-            private Array<Condition> conditions;
-            private IInstruction instruction;
+            public Array<Condition> conditions;
+            public IInstruction instruction;
 
             public void Execute(Runtime runtime)
             {
@@ -68,25 +185,50 @@ namespace Refactorio.game.scripting
                 }
             }
         }
-
-        private Dictionary<string, int> variables;
-        private Dictionary<int, int> memory;
-        private Dictionary<string, Array<ConditionalInstruction>> events;
-        private Array<string> callStack;
+        
+        public Dictionary<string, int> variables;
+        public Dictionary<int, int> memory;
+        public Dictionary<string, Array<ConditionalInstruction>> events;
+        public Dictionary<string, bool> flags;
+        public Array<string> callStack;
 
         private int GetVariable(string name)
         {
             return DictUtils.GetFromDict(variables, name, 0);
         }
 
+        private void SetVariable(string name, int value)
+        {
+            variables.Add(name, value);
+        }
+
+        private int GetMemory(int index)
+        {
+            return DictUtils.GetFromDict(memory, index, 0);
+        }
+
+        private void SetMemory(int index, int value)
+        {
+            memory.Add(index, value);
+        }
+
         public void RunEvent(string eventName)
         {
+            if (flags.ContainsKey(eventName))
+            {
+                flags.Add(eventName, true);
+                return;
+            }
             if (!events.TryGetValue(eventName, out var body)) return;
+            // Don't permit recursion by forbidding calling events that are already
+            // present in the call stack.
+            if (callStack.Contains(eventName)) return;
             callStack.Add(eventName);
             foreach (var instruction in body)
             {
                 instruction.Execute(this);
             }
+            callStack.Resize(callStack.Count - 1);
         }
     }
 }
